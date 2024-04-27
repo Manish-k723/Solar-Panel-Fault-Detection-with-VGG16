bb0a25388d179f103484410686e3502872509c01
Manish-k723 <manishkumar_me20b17_56@dtu.ac.in>
2024-04-27 20:26:07 +0530

    Updated files

--- model.py
+++ model.py
@@ -7,3 +7,10 @@ train_ds = tf.data.Dataset.load('train_ds')
 val_ds = tf.data.Dataset.load('val_ds')
 
 class_names= ['Bird-drop', 'Clean', 'Dusty', 'Electrical-damage', 'Physical-Damage', 'Snow-Covered']
+
+base_model = tf.keras.applications.VGG16(
+    include_top=False,
+    weights='imagenet',
+    input_shape=(img_height, img_width, 3)
+)
+base_model.trainable = False 
